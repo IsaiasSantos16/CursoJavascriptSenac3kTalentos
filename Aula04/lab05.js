da2171c3beb776948cae1b2cbdc1e8a61070974c
Isaias <isaias.santos1609@hotmail.com>
2023-08-11 20:24:51 -0300

    lab05

--- Aula04/lab05.js
+++ Aula04/lab05.js
@@ -1,28 +1,12 @@
-console.log(
-`
-Tabuada do 3
--------------
-3x1 = ${3*1} 	3x6 = ${3*6}
-3x2 = ${3*2} 	3x7 = ${3*7}
-3x3 = ${3*3} 	3x8 = ${3*8}
-3x4 = ${3*4} 	3x9 = ${3*9}
-3x5 = ${3*5} 	3x10 = ${3*10}
-`
-)
-console.log()
-console.log('-----------')
-console.log()
-console.log(
-`Operações de adição:
-2 + 2 	= ${2+2}
-4 + 1,5 = ${4+1.5}
-ab + cd = ${'ab' + 'cd'}
-ab + 2 	= ${'ab' + 2}
-
-Operações de subtração:
-2 - 2	= ${2-2}
-4 - 1,5	= ${4-1.5}
-ab - cd = ${'ab' - 'cd'}
-ab - 2  = ${'ab' - 2}
+console.log(`
+Usando 
+Crase
+É
+Possível
+Imprimir
+Texto
+Em
+Várias
+Linhas
 `
 )
