2194c49325ed0519e8fa7b79650ac7d6bbd9e843
Isaias <isaias.santos1609@hotmail.com>
2023-08-16 21:48:35 -0300

    Aula06 - Variáveis e primeiro projeto

--- Aula06/Ex6-EntradaDados.js
+++ Aula06/Ex6-EntradaDados.js
@@ -1,13 +1,12 @@
-const rl = require('readline-sync')
+console.log('Bem-vindo!\n');
 
-console.log('Bem-vindo!\n')
-
-const nome = rl.question('Qual o seu nome? \n')
-const sobrenome = rl.question('\nE o sobrenome? \n')
-const ano = rl.question('\nCerto ' + nome + ', qual o ano atual?\n')
-const idade = rl.question('\nOk, e qual sua idade? \n')
-const email = rl.question('\nPor fim, informe o seu e-mail: \n')
-const anoDeNascimento = Number(ano) - Number(idade)
+const rl = require('readline-sync');
+const nome = rl.question('Qual o seu nome?\n');
+const sobrenome = rl.question('\nE o sobrenome?\n');
+const ano = rl.question('\nCerto ' + nome + ', qual o ano atual?\n');
+const idade = rl.question('\nOk, e qual sua idade?\n');
+const email = rl.question('\nPor fim, informe o seu e-mail:\n');
+const anoDeNascimento = Number(ano) - Number(idade);
 
 console.log(`
 Nome: ${nome}
@@ -15,4 +14,4 @@ Sobrenome: ${sobrenome}
 Idade: ${idade}
 E-mail: ${email}
 Ano de Nascimento: ${anoDeNascimento}
-`)
+`);
